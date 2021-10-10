a728a1a2db9ab86ab5f55d3b424056d375a14b7b
mohasy <304182@naver.com>
2021-10-10 11:48:04 +0900

    step6 활성화 패널 모양 추가

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 const frame = document.querySelector("section");
 const list = frame.querySelectorAll("article");
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
 const len = list.length;
 const deg = 360/len;
 // console.log(deg);
+let num = 0; //바뀌는 값이기 때문에 let로 선언
 
 //패널의 갯수만큼 반복을 해서 일정 각도마다 회전
 for(let i =0; i<len; i++){
     list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
 }
+
+prev.addEventListener("click", () => {
+    frame.style.transform = `rotate(${deg* ++num}deg)`;
+});
+
+next.addEventListener("click", () => {
+    frame.style.transform = `rotate(${deg* --num}deg)`;
+});
