9abd97f8a945f21a8819aeb440ac10371495499d
mohasy <304182@naver.com>
2021-10-16 09:44:25 +0900

    step7 좌우버튼 클릭시 패널 활성화

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const prev = document.querySelector(".btnPrev");
 const next = document.querySelector(".btnNext");
 const len = list.length;
 const deg = 360/len;
-// console.log(deg);
-let num = 0; //바뀌는 값이기 때문에 let로 선언
+//바뀌는 값이기 때문에 let로 선언
+let num = 0; //박스의 초기 위치값설정
+let active = 0;
 
 //패널의 갯수만큼 반복을 해서 일정 각도마다 회전
 for(let i =0; i<len; i++){
@@ -14,8 +15,34 @@ for(let i =0; i<len; i++){
 
 prev.addEventListener("click", () => {
     frame.style.transform = `rotate(${deg* ++num}deg)`;
+        //현재 순번이 0이 되면 마지막 순번으로 초기화
+        // if(active == 0) {
+        //     active = len-1;
+        //     //마지막 순번이 아니면 1씩 감소
+        // } else {
+        //     active--;
+        // }
+
+        (active == 0)  ? active = len-1 : active--;
+        for(let el of list){
+            el.classList.remove("on");
+        }
+        list[active].classList.add("on");
 });
 
 next.addEventListener("click", () => {
     frame.style.transform = `rotate(${deg* --num}deg)`;
+    // //현재 순번이 마지막 순번이 되면 0으로 초기화
+    // if(active == len-1) {
+    //     active = 0;
+    //     //마지막 순번이 아니면 1씩 증가
+    // } else {
+    //     active++;
+    // }
+
+    (active == len-1) ? active = 0 : active++;
+    for(let el of list){
+        el.classList.remove("on");
+    }
+    list[active].classList.add("on");
 });
